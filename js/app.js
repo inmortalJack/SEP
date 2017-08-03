674d4c711754b4317882954c438a6b0c4fac28cc
Ricardo Ruíz Velazco <gral.vacio@gmail.com>
2017-08-03 12:44:13 -0500

    index

--- js/app.js
+++ js/app.js
@@ -5,13 +5,17 @@ var fs = require('fs');
 http.createServer(function (req, res) {
   var q = url.parse(req.url, true);
   var filename = "." + q.pathname;
+  console.log(filename)
+  if (filename == '/' || filename == './' ){
+    filename = './index.html';
+  }
 
 
   fs.readFile(filename, function(err, data) {
     console.log(q.pathname)
     if (err) {
       res.writeHead(404, {'Content-Type': 'text/html'});
-      return res.end("404 Yisus puto");
+      return res.end("404 La página no existe");
     }
     res.writeHead(200, {'Content-Type': 'text/html'});
     res.write(data);
