d9771eca9565aba9e675ca9454f8bb874f587b45
Ricardo Ruíz Velazco <gral.vacio@gmail.com>
2017-08-03 12:32:29 -0500

    yisus

--- js/app.js
+++ js/app.js
@@ -11,7 +11,7 @@ http.createServer(function (req, res) {
     console.log(q.pathname)
     if (err) {
       res.writeHead(404, {'Content-Type': 'text/html'});
-      return res.end("404 Not Found");
+      return res.end("404 Yisus puto");
     }
     res.writeHead(200, {'Content-Type': 'text/html'});
     res.write(data);
